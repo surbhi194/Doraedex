bc55393af4bc1dba74507d62fdd183df69c2e44c
Surbhi <surbhigupta100502@gmail.com>
2021-11-19 22:48:35 +0530

    done

---
+++ about/java.js
@@ -0,0 +1,85 @@
+console.log("doraemon");
+//  let search=document.getElementById('searchtxt');
+//     search.addEventListener("input",function(){
+//         let inputVal=search.nodeValue.toLowerCase();
+//         let noteCard=document.getElementsByClassName("card");
+//         Array.from(notecard).forEach(function(element){
+//             let cardTxt=document.getElementByTagName("P")[0].innerText;
+//             if(cardTxt.includes(inputVal)){
+//                 document.style.display="block";
+//             }
+//             else{
+//                 document.style.display="none";
+//             }
+//         })
+//      })
+// let cardTxt=document.getElementByTagName("P")[0].innerText;
+// if (searchTxt.length > 1) {
+//     const g = cardTxt.filter(
+//       (f) => JSON.stringify(f).toLowerCase().indexOf(value) !== -1
+//     )};
+    let html='';
+    let btn1 = document.getElementById("btn-1");
+btn1.addEventListener('click', function (e) {
+html=`<p><b>ANYWHERE DOOR</b><br>The range of destinations can vary, and is heavily affected by the time period it is used in.</p>`
+let notesElm = document.getElementById("gadget");
+notesElm.innerHTML = html;
+});  
+let btn2 = document.getElementById("btn-2");
+btn2.addEventListener('click', function (e) {
+html=`<p><b>COPYING TOAST</b><br>To use the Copying Toast, one should press one side of the toast on a book with either text or pictures. The text or picture will then be 'printed' (in the bread's case, 'sticks onto it') on the toast. Eating the toast later will allow the eater to remember whatever content that has been printed on the toast. </p>`
+let notesElm = document.getElementById("gadget");
+notesElm.innerHTML = html;
+});  
+let btn3 = document.getElementById("btn-3");
+btn3.addEventListener('click', function (e) {
+html=`<p><b>TIME CLOTH</b><br>The Time Cloth is able to advance/regress the state of a target by causing a temporal distortion that goes forwards/backwards by covering an item with its blue/red side. </p>`
+let notesElm = document.getElementById("gadget");
+notesElm.innerHTML = html;
+});  
+let btn4 = document.getElementById("btn-4");
+btn4.addEventListener('click', function (e) {
+html=`<p><b>TAKE COPTER</b><br>Users can use it to fly from one place to another by just thinking of the direction they wanna move in.It can carry heavy loads.
+The speed can be customized by the user.</p>`
+let notesElm = document.getElementById("gadget");
+notesElm.innerHTML = html;
+});  
+let btn5 = document.getElementById("btn-5");
+btn5.addEventListener('click', function (e) {
+html=`<p><b>TIME MACHINE</b><br>Time Machine can be used to travel from one dimension to another. It can be used to travel to future and past.
+It can also be used to travel to several locations existing in the same timeline.</p>`
+let notesElm = document.getElementById("gadget");
+notesElm.innerHTML = html;
+});  
+let btn6 = document.getElementById("btn-6");
+btn6.addEventListener('click', function (e) {
+html=`<p><b>BIG LIGHT</b><br>When used, Big Light fires a laser which temporarily increases the mass and space occupied by the target to a certain extent for an unspecified amount of time. </p>`
+let notesElm = document.getElementById("gadget");
+notesElm.innerHTML = html;
+});  
+let btn7 = document.getElementById("btn-7");
+btn7.addEventListener('click', function (e) {
+html=`<p><b>AIR CANNON</b><br>The wielder puts the Air Cannon on his/her arm and shouts 'Bang!' then a huge amount of air comes out of the cannon, blowing off the target. Although the number of bullets is unlimited, it is still unusable when it runs out of battery. </p>`
+let notesElm = document.getElementById("gadget");
+notesElm.innerHTML = html;
+});  
+let btn8 = document.getElementById("btn-8");
+btn8.addEventListener('click', function (e) {
+html=`<p><b>COTTON CANDY CLOUD MAKER</b><br>To use this gadget, the user must put it into a cloud, then add their favorite flavors, foods and even the drinks. Finally, press the button on the gadget to create it and eat these clouds.
+
+This cotton candy is almost absent when eating, because it is naturally cloudy so users can eat as much as they like. </p>`
+let notesElm = document.getElementById("gadget");
+notesElm.innerHTML = html;
+});  
+let btn9 = document.getElementById("btn-9");
+btn9.addEventListener('click', function (e) {
+html=`<p><b>COPY ROBOT</b><br>To activate it, the user must first push the button on the Copy Robot and it will transform into the user it was being held in. The robot will have the same look and personality as the user and the only difference between the robot and the user is the robot's reddish (or sometimes pinkish) nose. However, if the robot gets its nose hurt, its personality will change into a different one (a kind robot will become mean if it gets its head hurt). To deactivate it, the user must push the button on the robot's nose. </p>`
+let notesElm = document.getElementById("gadget");
+notesElm.innerHTML = html;
+});
+   let btn10 = document.getElementById("btn-10");
+btn10.addEventListener('click', function (e) {
+html=`<p><b>DELIVERY RACKET</b><br>The user must say first the destination of the specific object. Then he/she hits the object to its destination. The gadget can hit anything no matter of its size.  </p>`
+let notesElm = document.getElementById("gadget");
+notesElm.innerHTML = html;
+});  
